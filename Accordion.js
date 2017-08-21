6c2f418f5038a41cc50f10d65beb04f1d8072b92
sushantsahu <sahu.sushant115@gmail.com>
2017-08-21 14:53:27 +0530

    changes reverted

--- Accordion.js
+++ Accordion.js
@@ -1,19 +1,13 @@
-import React, {
-  Component,
-} from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-
-import {
-  View,
-  TouchableHighlight,
-} from 'react-native';
-
+import { View, TouchableHighlight } from 'react-native';
 import Collapsible from './Collapsible';
+import { ViewPropTypes } from './config';
 
 const COLLAPSIBLE_PROPS = Object.keys(Collapsible.propTypes);
-const VIEW_PROPS = Object.keys(View.propTypes);
+const VIEW_PROPS = Object.keys(ViewPropTypes);
 
-class Accordion extends Component {
+export default class Accordion extends Component {
   static propTypes = {
     sections: PropTypes.array.isRequired,
     renderHeader: PropTypes.func.isRequired,
@@ -27,53 +21,52 @@ class Accordion extends Component {
       PropTypes.bool, // if false, closes all sections
       PropTypes.number, // sets index of section to open
     ]),
-    activeSections: PropTypes.array.isRequired,
     underlayColor: PropTypes.string,
+    touchableComponent: PropTypes.func,
+    touchableProps: PropTypes.object,
   };
 
   static defaultProps = {
     underlayColor: 'black',
+    touchableComponent: TouchableHighlight,
   };
 
   constructor(props) {
     super(props);
+
     // if activeSection not specified, default to initiallyActiveSection
     this.state = {
-      activeSections: [],
-      activeSection: props.activeSection !== undefined ? props.activeSection : props.initiallyActiveSection,
+      activeSection:
+        props.activeSection !== undefined
+          ? props.activeSection
+          : props.initiallyActiveSection,
     };
   }
 
-  _toggleSection(section) {
-    const activeSections = this.state.activeSections;
-    const index = activeSections.indexOf(section);
-    let activeSection = section;
-    if (index > -1) {
-      activeSection = false;
-      activeSections.splice(index, 1);
-    } else {
-      activeSections.push(section);
-    }
-    this.setState({ activeSections });
-    this.setState({ activeSection });
-
-    if (this.props.onChange) {
-      this.props.onChange(activeSection);
+  componentWillReceiveProps(nextProps) {
+    if (nextProps.activeSection !== undefined) {
+      this.setState({
+        activeSection: nextProps.activeSection,
+      });
     }
   }
 
-  componentWillReceiveProps(nextProps) {
-    if (nextProps.activeSections !== undefined) {
-      this.setState({
-        activeSections: nextProps.activeSections,
-      });
+  _toggleSection(section) {
+    const activeSection =
+      this.state.activeSection === section ? false : section;
+
+    if (this.props.activeSection === undefined) {
+      this.setState({ activeSection });
+    }
+    if (this.props.onChange) {
+      this.props.onChange(activeSection);
     }
   }
 
   render() {
     let viewProps = {};
     let collapsibleProps = {};
-    Object.keys(this.props).forEach((key) => {
+    Object.keys(this.props).forEach(key => {
       if (COLLAPSIBLE_PROPS.indexOf(key) !== -1) {
         collapsibleProps[key] = this.props[key];
       } else if (VIEW_PROPS.indexOf(key) !== -1) {
@@ -81,21 +74,36 @@ class Accordion extends Component {
       }
     });
 
+    const Touchable = this.props.touchableComponent;
+
     return (
       <View {...viewProps}>
-      {this.props.sections.map((section, key) => (
-        <View key={key}>
-          <TouchableHighlight onPress={() => this._toggleSection(key)} underlayColor={this.props.underlayColor}>
-            {this.props.renderHeader(section, key, this.state.activeSections.indexOf(key) !== -1)}
-          </TouchableHighlight>
-          <Collapsible collapsed={this.state.activeSections.indexOf(key) === -1} {...collapsibleProps}>
-            {this.props.renderContent(section, key, this.state.activeSection === key)}
-          </Collapsible>
-        </View>
-      ))}
+        {this.props.sections.map((section, key) =>
+          <View key={key}>
+            <Touchable
+              onPress={() => this._toggleSection(key)}
+              underlayColor={this.props.underlayColor}
+              {...this.props.touchableProps}
+            >
+              {this.props.renderHeader(
+                section,
+                key,
+                this.state.activeSection === key
+              )}
+            </Touchable>
+            <Collapsible
+              collapsed={this.state.activeSection !== key}
+              {...collapsibleProps}
+            >
+              {this.props.renderContent(
+                section,
+                key,
+                this.state.activeSection === key
+              )}
+            </Collapsible>
+          </View>
+        )}
       </View>
     );
   }
 }
-
-module.exports = Accordion;
